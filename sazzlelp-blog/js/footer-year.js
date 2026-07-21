50ef6ca5f7a1b640364f3ea5d9ddd06cffab35e4
Simon <simhenk43@gamil.com>
2026-07-21 21:47:11 +0200

    Doppelten Ordner entfernt

--- sazzlelp-blog/js/footer-year.js
+++
@@ -1,6 +0,0 @@
-// Trägt automatisch das aktuelle Jahr im Footer ein
-(function () {
-  const el = document.querySelector('[data-current-year]');
-  if (!el) return;
-  el.textContent = new Date().getFullYear();
-})();
